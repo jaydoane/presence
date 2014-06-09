6f57614e7c44c24d44b77e8267ec331c8d856797
Jay Doane <jay.s.doane@gmail.com>
2014-06-08 23:09:32 -0700

    Remove comments

--- apps/session/src/session.erl
+++ apps/session/src/session.erl
@@ -21,8 +21,6 @@ stop_count(Type, Count) ->
     [stop(gp:where({Type,N})) || N <- lists:seq(1,Count)].
 
 subscribe(TransmitterTid, ListenerTid) ->
-    %% gp:cast(TransmitterTid, {add_listener, ListenerTid}),
-    %% gp:cast(ListenerTid, {add_transmitter, TransmitterTid}).
     ok = gp:call(TransmitterTid, {add_listener, ListenerTid}),
     ok = gp:call(ListenerTid, {add_transmitter, TransmitterTid}).
 

b91a3dd8d01de4da59697c576f82e4ef4a6c4daa
Jay Doane <jay.s.doane@gmail.com>
2014-06-23 15:11:15 -0700

    Make lager behave better running unit tests

--- apps/session/include/session.hrl
+++ apps/session/include/session.hrl
@@ -5,6 +5,8 @@
                 notifications=[],
                 type_state}).
 
+-compile([{parse_transform, lager_transform}]).
+
 -define(info(Format, Args),
         lager:info("~s", [io_lib:format(Format, Args)])).
 

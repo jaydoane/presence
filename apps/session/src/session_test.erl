00eb630648b5ee6fb74a48fc385e27f8d0d75eb4
Jay Doane <jay.s.doane@gmail.com>
2014-06-23 19:38:09 -0700

    Use synchronized session:start & session:stop functions in test

--- apps/session/src/session_test.erl
+++ apps/session/src/session_test.erl
@@ -20,11 +20,11 @@ session_test_() ->
 t_session_lifecycle() ->
     S1 = {session,1},
     S2 = {session,2},
-    {ok, Pid1} = session_sup:start_child(S1),
-    {ok, Pid2} = session_sup:start_child(S2),
+    {ok, Pid1} = session:start(S1),
+    {ok, Pid2} = session:start(S2),
     ?assertEqual(Pid1, gp:whereis(S1)),
     ?assertEqual(Pid2, gp:whereis(S2)),
-    session:subscribe(S1, S2),
+    ok = session:subscribe(S1, S2),
     ?assertEqual([S2], gp:call(S1, listeners)),
     ?assertEqual([S1], gp:call(S2, transmitters)),
     gp:cast(S1, {notify_listeners, first_post}),
@@ -32,8 +32,7 @@ t_session_lifecycle() ->
     gp:cast(S1, {notify_listeners, second_post}),
     sys:get_status(Pid2), % in order to sync
     ?assertEqual([second_post, first_post], gp:call(S2, notifications)),
-    ok = session_sup:stop_child(Pid2),
-    sys:get_status(Pid1), % in order to sync
+    ok = session:stop(S2),
     ?assertEqual([], gp:call(S1, listeners)),
     ok.
 
